4c24ba9fb3061c954d1adfbf43dc1db874d17b82
muzappar <1424939452@qq.com>
2022-06-23 17:27:36 +0800

    新的分支：木扎帕

--- src/test/java/com/example/mybatisplus/MybatisplusApplicationTests.java
+++ src/test/java/com/example/mybatisplus/MybatisplusApplicationTests.java
@@ -4,7 +4,7 @@ import com.example.mybatisplus.mapper.StudentMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//
 @SpringBootTest//加载ioc容器
 class MybatisplusApplicationTests {
 
