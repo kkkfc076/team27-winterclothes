08c7162e42da3e0ff2a5032344fe52b69ba04692
坤凡 陈 <535742729@qq.com>
2022-06-23 16:59:15 +0800

    框架提交尝试1

--- src/main/java/com/example/mybatisplus/common/utls/SessionUtils.java
+++ src/main/java/com/example/mybatisplus/common/utls/SessionUtils.java
@@ -1,6 +1,5 @@
 package com.example.mybatisplus.common.utls;
 
-import com.example.mybatisplus.model.domain.Admin;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
@@ -14,11 +13,4 @@ public class SessionUtils {
         return attr.getRequest().getSession(true); // true == allow create
     }
 
-    public static Admin getCurrentUserInfo() {
-        return (Admin) session().getAttribute(USERKEY);
-    }
-
-    public static void saveCurrentUserInfo(Admin admin) {
-        session().setAttribute(USERKEY, admin);
-    }
 }
