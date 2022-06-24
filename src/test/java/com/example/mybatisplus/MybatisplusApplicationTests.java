f317c6365b0f66539f94aa5a4f5e8330df8ef4de
muzappar <1424939452@qq.com>
2022-06-24 21:06:07 +0800

    列表接口

--- src/test/java/com/example/mybatisplus/MybatisplusApplicationTests.java
+++ src/test/java/com/example/mybatisplus/MybatisplusApplicationTests.java
@@ -1,6 +1,12 @@
 package com.example.mybatisplus;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.example.mybatisplus.mapper.ManagerApplicationMapper;
 import com.example.mybatisplus.mapper.StudentMapper;
+import com.example.mybatisplus.model.domain.Applicationform;
+import com.example.mybatisplus.model.domain.ManagerApplication;
+import com.example.mybatisplus.model.dto.PageDTO;
+import com.example.mybatisplus.service.ManagerApplicationService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,13 +14,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest//加载ioc容器
 class MybatisplusApplicationTests {
 
-@Autowired
+    @Autowired
     StudentMapper studentMapper;
+    @Autowired
+    ManagerApplicationMapper managerApplicationMapper;
 
-@Test
-    public void get(){
-        String pwd=studentMapper.getPwd(706);
+    @Test
+    public void get() {
+        String pwd = studentMapper.getPwd(706);
     }
 
-
+    @Test
+    public void ceshi() {
+        ManagerApplication mapp=new ManagerApplication();
+        mapp=managerApplicationMapper.selectById(1);
+        System.out.println(mapp);
+    }
 }
