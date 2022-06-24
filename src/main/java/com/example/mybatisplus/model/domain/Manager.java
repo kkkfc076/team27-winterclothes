c238e20baf643adc95ab189c68ba1376c6d45c04
坤凡 陈 <535742729@qq.com>
2022-06-24 09:48:24 +0800

    管理登录

---
+++ src/main/java/com/example/mybatisplus/model/domain/Manager.java
@@ -0,0 +1,53 @@
+package com.example.mybatisplus.model.domain;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.io.Serializable;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * ???еĹ???Ա?˺ţ?????ϵͳ????Ա??????Ա??ѧԺ??ѧУ?û?
+ * </p>
+ *
+ * @author team27
+ * @since 2022-06-24
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@ApiModel(value="Manager对象", description="???еĹ???Ա?˺ţ?????ϵͳ????Ա??????Ա??ѧԺ??ѧУ?û?")
+public class Manager extends Model<Manager> {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    private Integer mid;
+
+    private String pwd;
+
+    private String name;
+
+    private Integer level;
+
+    private String major;
+
+    private Integer grade;
+
+    private Integer tclass;
+
+
+    @Override
+    protected Serializable pkVal() {
+        return this.id;
+    }
+
+}
